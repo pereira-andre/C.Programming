7ff0ae81d813d77fc21e2307820aff487849b4d2
pereira-andre <118055264+pereira-andre@users.noreply.github.com>
2023-02-22 18:13:07 +0000

    Create program prints the multiplication table of an int.c

---
+++ Section 16 - Loops Exercises/program prints the multiplication table of an int.c
@@ -0,0 +1,32 @@
+//program prints the multiplication table of an int
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    
+    int n,i,t=0;
+    printf("Please insert a number: \n");
+    scanf("%d",&n);
+    
+    if (n==0)
+        printf("0\n");
+        
+        else if (n<=10)
+        for(i = 1; i<=10;i++)
+    {
+        t=i*n;
+        printf("%d * %d = %d\n",i,n,t);
+    }
+    
+    else
+        for(i = 1; i<=n;i++)
+    {
+        t=i*n;
+        printf("%d * %d = %d\n",i,n,t);
+    }
+    
+	
+    return 0;
+}
