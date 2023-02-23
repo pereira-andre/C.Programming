5e166fb7dbe6a6e46fdea121c51301fefb014667
pereira-andre <118055264+pereira-andre@users.noreply.github.com>
2023-02-23 16:48:25 +0000

    Create Print a rectangle triangle with numbers n.c

---
+++ Section 15 - Starting with Loops/Print a rectangle triangle with numbers n.c
@@ -0,0 +1,24 @@
+//Print a rectangle triangle with numbers n
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    
+    int n,i,j;
+    printf("Please insert the number for the pattern: \n");
+    scanf("%d",&n);
+    
+    if (n ==0)
+        printf("Try again!");
+    else
+        for (i=1; i <= n; i++)
+	{
+		for (j=1; j<=i ;j++)
+		        printf("%d",i);
+		        printf("\n");
+		
+	}
+    return 0;
+}
