e82895203597d7ee4969bc245f67e32e4ba96887
pereira-andre <118055264+pereira-andre@users.noreply.github.com>
2023-02-23 16:50:21 +0000

    Create Print mensagem 1000x times.c

---
+++ Section 15 - Starting with Loops/Print mensagem 1000x times.c
@@ -0,0 +1,17 @@
+//Print mensagem 1000x times
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    
+    int i;
+    
+        for (i = 1; i <= 1000; i++)
+	{
+		printf("I'm sorry I did not listen to my teacher, will do better next time! \n ");
+	}
+	
+    return 0;
+}
